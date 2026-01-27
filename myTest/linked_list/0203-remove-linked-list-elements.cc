eb4700fd93bc4bae3310566fa030854c98be9907
ding.xingyu <ding.xingyu@byd.com>
2026-01-27 17:57:57 +0800

    20260127学习

--- myTest/linked_list/0203-remove-linked-list-elements.cc
+++ myTest/linked_list/0203-remove-linked-list-elements.cc
@@ -24,18 +24,68 @@
 #include <iostream>
 
 // Definition for singly-linked list.
-struct ListNode {
+// struct ListNode {
+//     int val;
+//     ListNode *next;
+//     ListNode() : val(0), next(nullptr) {}
+//     ListNode(int x) : val(x), next(nullptr) {}
+//     ListNode(int x, ListNode *next) : val(x), next(next) {}
+// };
+
+// 手打一遍链表定义
+struct ListNode
+{
     int val;
     ListNode *next;
     ListNode() : val(0), next(nullptr) {}
     ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    ListNode(int x, ListNode *next) : val(0), next(next) {}
 };
+
 
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        // 请在此处实现你的代码
+        // 1.直接使用原来的链表来进行移除节点操作
+        // // 需要分别处理头和非头节点
+        // // 删除头节点：如果要删除的值等于
+        // while (head != NULL && head->val == val) { // 注意不是if，因为可能从头节点开始连续多个节点都是要删除的
+        //     ListNode* tmp = head;
+        //     head = head->next; // 让头节点等于下一个链表节点
+        //     delete tmp; 
+        // }
+
+        // // 删除非头节点
+        // ListNode* cur = head;
+        // while (cur != NULL && cur->next != NULL) { // 当前指针不是空指针，并且下一指针也非空指针
+        //     if (cur->next->val == val) { // 
+        //         ListNode* tmp = cur->next;
+        //         cur->next = cur->next->next;
+        //         delete tmp;
+        //     } else {
+        //         cur = cur->next; //
+        //     }
+        // }
+
+        // // 返回一个链表，返回头head就可以了
+        // return head;
+
+        // 2.设置一个虚拟头节点
+        ListNode* dummyHead = new ListNode(0); // 设置一个虚拟头节点，传入val=0
+        dummyHead->next = head; // 虚拟头节点指向head，方便后续做删除操作
+        ListNode* cur = dummyHead; // 将当前的节点从虚拟头开始进行
+        while (cur->next != NULL) {
+            if (cur->next->val == val) {
+                ListNode* tmp = cur->next;
+                cur->next = cur->next->next;
+                delete tmp; // 删除释放内存
+            } else {
+                cur = cur->next;
+            }
+        }
+        head = dummyHead->next; 
+        delete dummyHead; // 记得要删除释放内存
+        return head;
         
     }
 };
@@ -55,7 +105,7 @@ int main() {
     int val1 = 6;
     ListNode* result1 = sol.removeElements(head1, val1);
     std::cout << "Test Case 1: val = 6, Result = ";
-    while (result1) {
+    while (result1) { // 只要不指向最后一个空节点，就while继续
         std::cout << result1->val << " ";
         result1 = result1->next;
     }
@@ -75,6 +125,93 @@ int main() {
     int val3 = 7;
     ListNode* result3 = sol.removeElements(head3, val3);
     std::cout << "Test Case 3: val = 7, Result = (empty) (Expected: empty)" << std::endl;
+
+    // 测试用例 4: 删除不存在的值
+    ListNode* head4 = new ListNode(1);
+    head4->next = new ListNode(2);
+    head4->next->next = new ListNode(3);
+    int val4 = 4;
+    ListNode* result4 = sol.removeElements(head4, val4);
+    std::cout << "Test Case 4: val = 4, Result = ";
+    while (result4) {
+        std::cout << result4->val << " ";
+        result4 = result4->next;
+    }
+    std::cout << "(Expected: 1 2 3)" << std::endl;
+
+    // 测试用例 5: 删除中间的元素
+    ListNode* head5 = new ListNode(1);
+    head5->next = new ListNode(2);
+    head5->next->next = new ListNode(3);
+    head5->next->next->next = new ListNode(2);
+    head5->next->next->next->next = new ListNode(4);
+    int val5 = 2;
+    ListNode* result5 = sol.removeElements(head5, val5);
+    std::cout << "Test Case 5: val = 2, Result = ";
+    while (result5) {
+        std::cout << result5->val << " ";
+        result5 = result5->next;
+    }
+    std::cout << "(Expected: 1 3 4)" << std::endl;
+
+    // 测试用例 6: 删除头节点
+    ListNode* head6 = new ListNode(2);
+    head6->next = new ListNode(2);
+    head6->next->next = new ListNode(3);
+    int val6 = 2;
+    ListNode* result6 = sol.removeElements(head6, val6);
+    std::cout << "Test Case 6: val = 2, Result = ";
+    while (result6) {
+        std::cout << result6->val << " ";
+        result6 = result6->next;
+    }
+    std::cout << "(Expected: 3)" << std::endl;
+
+    // 测试用例 7: 删除尾节点
+    ListNode* head7 = new ListNode(1);
+    head7->next = new ListNode(2);
+    head7->next->next = new ListNode(3);
+    head7->next->next->next = new ListNode(2);
+    int val7 = 2;
+    ListNode* result7 = sol.removeElements(head7, val7);
+    std::cout << "Test Case 7: val = 2, Result = ";
+    while (result7) {
+        std::cout << result7->val << " ";
+        result7 = result7->next;
+    }
+    std::cout << "(Expected: 1 3)" << std::endl;
+
+    // 测试用例 8: 只有一个节点且匹配
+    ListNode* head8 = new ListNode(5);
+    int val8 = 5;
+    ListNode* result8 = sol.removeElements(head8, val8);
+    std::cout << "Test Case 8: val = 5, Result = (empty) (Expected: empty)" << std::endl;
+
+    // 测试用例 9: 只有一个节点且不匹配
+    ListNode* head9 = new ListNode(5);
+    int val9 = 6;
+    ListNode* result9 = sol.removeElements(head9, val9);
+    std::cout << "Test Case 9: val = 6, Result = ";
+    while (result9) {
+        std::cout << result9->val << " ";
+        result9 = result9->next;
+    }
+    std::cout << "(Expected: 5)" << std::endl;
+
+    // 测试用例 10: 多个连续的val
+    ListNode* head10 = new ListNode(1);
+    head10->next = new ListNode(2);
+    head10->next->next = new ListNode(2);
+    head10->next->next->next = new ListNode(2);
+    head10->next->next->next->next = new ListNode(3);
+    int val10 = 2;
+    ListNode* result10 = sol.removeElements(head10, val10);
+    std::cout << "Test Case 10: val = 2, Result = ";
+    while (result10) {
+        std::cout << result10->val << " ";
+        result10 = result10->next;
+    }
+    std::cout << "(Expected: 1 3)" << std::endl;
 
     return 0;
 }
--- myTest/linked_list/0707-design-linked-list.cc
+++ myTest/linked_list/0707-design-linked-list.cc
@@ -39,36 +39,108 @@ struct ListNode {
 };
 
 class MyLinkedList {
+private: // 定义私有变量,全局变量，然后可以在构造函数中进行初始化
+    int _size = 0;
+    ListNode* _dummyHead;
+
 public:
+    // 使用虚拟头节点的方式进行
+
+    // 初始化链表：构造函数
     MyLinkedList() {
-        // 请在此处实现构造函数
+        _dummyHead = new ListNode(0);
+        _size = 0; 
         
     }
     
+    // 获取到第index个节点数值，如果index是非法数值直接返回-1， 注意index是从0开始的，第0个节点就是头结点
     int get(int index) {
-        // 请在此处实现
+        if (index > (_size - 1) || index < 0) {
+            return -1;
+        }
+        ListNode* cur = _dummyHead->next;
+        while (index--) { // 先判断while内部，再执行index--;index =2 , 2进去，index=1，1进去，index=0,0结束，因为cur是从头开始的，所以0结束是合理的，可以通过cout输出index来快速查看。
+            cur = cur->next;
+        }
+        return cur->val;
         
     }
     
+    // 在链表最前面插入一个节点，插入完成后，新插入的节点为链表的新的头结点
     void addAtHead(int val) {
-        // 请在此处实现
+        ListNode* newNode = new ListNode(val);
+        newNode->next = _dummyHead->next;
+        _dummyHead->next = newNode;
+        _size++;
         
     }
     
+    // 在链表最后面添加一个节点
     void addAtTail(int val) {
-        // 请在此处实现
+        ListNode* newNode = new ListNode(val);
+        ListNode* cur = _dummyHead;
+        while (cur->next != nullptr) { //结束的时候是cur在链表的最后一个位置
+            cur = cur->next;
+        } 
+        cur->next = newNode;
+        _size++; // 添加节点后要加加size
         
     }
     
+    // 在第index个节点之前插入一个新节点，例如index为0，那么新插入的节点为链表的新头节点。
+    // 如果index 等于链表的长度，则说明是新插入的节点为链表的尾结点
+    // 如果index大于链表的长度，则返回空
+    // 如果index小于0，则在头部插入节点
     void addAtIndex(int index, int val) {
-        // 请在此处实现
+        if(index > _size) return; // 大于链表长度，返回空
+        if(index < 0) index = 0;
+        ListNode* newNode = new ListNode(val);
+        ListNode* cur = _dummyHead;
+        while(index--) {
+            cur = cur->next;
+        }
+        // while循环结束的时候 cur等于index所在的位置
+        newNode->next = cur->next;
+        cur->next = newNode;
+        _size++;
+
         
     }
     
+    // 删除第index个节点，如果index 大于等于链表的长度，直接return，注意index是从0开始的
     void deleteAtIndex(int index) {
-        // 请在此处实现
+        if (index >= _size || index < 0) {
+            return;
+        }
+        ListNode* cur = _dummyHead;
+        while (index--) {
+            cur = cur->next;
+        }
+        ListNode* tmp = cur->next;
+        cur->next = cur->next->next;
+        delete tmp;
+
+        //delete命令指示释放了tmp指针原本所指的那部分内存，
+        //被delete后的指针tmp的值（地址）并非就是NULL，而是随机值。也就是被delete后，
+        //如果不再加上一句tmp=nullptr,tmp会成为乱指的野指针
+        //如果之后的程序不小心使用了tmp，会指向难以预想的内存空间
+        
+        tmp=nullptr;
+        _size--;
+
         
     }
+
+    // 打印链表：
+    void printLinkedList() {
+        ListNode* cur = _dummyHead;
+        while (cur->next != nullptr) {
+            std::cout << cur->next->val << " ";
+            cur = cur->next;
+        }
+        std::cout << std::endl;
+    }
+    
 };
 
 int main() {
